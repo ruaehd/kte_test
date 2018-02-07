eff3896ea0d1f0159a54cd7948c34b47b6ed1b87
ruaehd <ruaehdehddk@naver.com>
2018-02-07 16:57:45 +0900

    20180207

--- sts1/src/main/java/com/kte/mvc/HomeController.java
+++ sts1/src/main/java/com/kte/mvc/HomeController.java
@@ -8,8 +8,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.kte.mvc.exception.ResourceNotFoundException;
 
 /**
  * Handles requests for the application home page.
@@ -19,7 +23,23 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public String notFound() {
+		//페이지 없을 경우 표시될 jsp파일명
+		return "v1_pagenotfound";
+	}
+	
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String home1(@PathVariable("name")String name) {
+		if(name.equals("home")) {
+			return "home";
+		}
+		else {
+			throw new ResourceNotFoundException();
+		}
+	}
+	/**	
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
---
+++ sts1/src/main/webapp/WEB-INF/views/v1_pagenotfound.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>페이지 오류</title>
+	<link href="resources/css/bootstrap.css" rel="stylesheet" />
+</head>
+<body>
+	<h1>페이지 오류</h1>
+	<label>페이지가 존재하지 않습니다</label>
+
+</body>
+</html>
